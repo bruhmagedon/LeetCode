8f5aed0220d6fb3a1842471a38f141c29765a64c
Maxim <schubert.walk@yandex.ru>
2025-10-13 10:47:06 +0300

    1920 easy

---
+++ projects/1920-build-array-from-permutation/src/index.ts
@@ -0,0 +1,16 @@
+// 1920. Build Array from Permutation
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+function buildArray(nums: number[]): number[] {
+    const ans: number[] = [];
+
+    nums.forEach((item, _, array) => {
+        ans.push(array[item]) //nums[nums[i]]
+    })
+    // 1 cпособ
+    return ans;
+
+    // 2 cпособ
+    return nums.map((item, i) => nums[item])
+};
